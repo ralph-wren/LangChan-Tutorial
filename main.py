426d77ae99351efbb0e68618c10f0cae5302314e
Ralph Wren <ralph_jungle@163.com>
2025-12-15 00:06:08 +0800

    1

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+import sys
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import langchain
+import openai
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(  langchain.__version__)
+    print(openai.__version__)
+    print(sys.version_info)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
